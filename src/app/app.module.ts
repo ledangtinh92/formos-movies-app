d5e2d3ff810bfeaa36fd51d7ec84f28afb943ae0
tinh.le <tinh.le@formos.com>
2023-08-16 18:13:16 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from 'src/app/layouts/main/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FooterComponent} from "src/app/layouts/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layouts/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+
+  onConnectLinkOpenNewTab(linkNumber: number): void {
+    const linkLinkedInUrl = 'https://www.linkedin.com/company/27063000/';
+    const linkTwitterUrl = 'https://twitter.com/counsl_inc';
+    const linkFaceUrl = 'https://www.facebook.com/counsl.inc/';
+    let currentLink = linkLinkedInUrl;
+    if (linkNumber === 1) {
+      currentLink = linkTwitterUrl;
+    } else if (linkNumber === 2) {
+      currentLink = linkFaceUrl;
+    }
+    window.open(currentLink, '_blank');
+  }
+}
