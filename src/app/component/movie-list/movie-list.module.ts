1438505bac90aad7a1077a00fe705b19ebfcabf9
tinh.le <tinh.le@formos.com>
2023-08-25 16:36:04 +0700

    clear comment

--- src/app/component/movie-list/movie-list.module.ts
+++ src/app/component/movie-list/movie-list.module.ts
@@ -4,22 +4,15 @@ import {movielstState} from "./movie-list.route";
 import {SharedLibsModule} from "../../shared/shared-libs.module";
 import {MovieListComponent} from "./movie-list.component";
 import {NgbRating} from "@ng-bootstrap/ng-bootstrap";
-import {IMAGE_LOADER, ImageLoaderConfig, NgOptimizedImage, provideImgixLoader} from "@angular/common";
+import {NgOptimizedImage, provideImgixLoader} from "@angular/common";
+import {environment} from "../../environment/environment";
 
 @NgModule({
   imports: [SharedLibsModule, RouterModule.forChild(movielstState), NgbRating, NgOptimizedImage],
   declarations: [MovieListComponent],
   providers: [
-    provideImgixLoader("https://image.tmdb.org/t/p/"),
+    provideImgixLoader(environment.SERVER_IMAGES_URL),
   ],
-  // providers: [
-  //   {
-  //     provide: IMAGE_LOADER,
-  //     useValue: (config: ImageLoaderConfig) => {
-  //       return `https://image.tmdb.org/t/p/`;
-  //     }
-  //   },
-  // ]
 })
 
 export class MovieListModule {
