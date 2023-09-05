d4d3a7f817ccae8d987559678de8395f6caebc62
tinh.le <tinh.le@formos.com>
2023-09-05 17:49:48 +0700

    update

--- src/app/component/mini-games/battlesship-game/battleship-game.component.ts
+++ src/app/component/mini-games/battlesship-game/battleship-game.component.ts
@@ -1,7 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {BattleshipGameService} from "@component/mini-games/battlesship-game/battleship-game.service";
-import {BehaviorSubject, combineLatest, debounceTime, Observable, scan, Subject} from "rxjs";
-import {combineLatestInit} from "rxjs/internal/observable/combineLatest";
+import {BehaviorSubject, combineLatest, debounceTime, scan, Subject} from "rxjs";
 
 @Component({
   selector: 'app-battleship-game',
@@ -9,51 +8,53 @@ import {combineLatestInit} from "rxjs/internal/observable/combineLatest";
   styleUrls: ['./battleship-game.component.scss']
 })
 export class BattleShipGameComponent implements OnInit, AfterViewInit {
-  playersYouBoards: number[][] = [];
-  shipYouRecords: number = 50;
-  youRecords: number = 0;
-  youTurn: boolean = true;
-  playersComputerBoards: number[][] = [];
-  shipComputerRecords: number = 50;
-  computerRecords: number = 0;
+  playerBoard: number[][] = [];
+  ComputerBoard: number[][] = [];
+  playerPoints: number = 50;
+  computerPoints: number = 50;
+
+  playerScores: number = 0;
+  computerScores: number = 0;
+  playerTurn: boolean = true;
+
+
   turnObservable: Subject<boolean>;
-
-
   computerRecordOb: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   playRecordOb: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  turnOb: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
+  playerTurnOb: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private battleshipGameService: BattleshipGameService) {
     this.turnObservable = new Subject<boolean>();
   }
 
   ngOnInit(): void {
-    this.playersYouBoards = this.battleshipGameService.createTwoDimArray(12, 12);
-    this.playersComputerBoards = this.battleshipGameService.createTwoDimArray(12, 12);
-    this.battleshipGameService.computerSettingShipLocation(this.playersComputerBoards,this.shipComputerRecords);
-    this.battleshipGameService.computerSettingShipLocation(this.playersYouBoards,49);
-    this.shipComputerRecords = 0;
-    this.shipYouRecords = 1;
+    this.playerBoard = this.battleshipGameService.createTwoDimArray(12, 12);
+    this.ComputerBoard = this.battleshipGameService.createTwoDimArray(12, 12);
+    this.battleshipGameService.computerSettingShipLocation(this.ComputerBoard, this.computerPoints);
+    this.battleshipGameService.computerSettingShipLocation(this.playerBoard, 49);
+    this.computerPoints = 0;
+    this.playerPoints = 1;
 
     this.turnObservable.pipe(debounceTime(1000)).subscribe(value => {
-        this.computerPlay();
+      this.computerPlay();
     })
 
-    combineLatest([this.computerRecordOb, this.playRecordOb, this.turnOb])
+    combineLatest([this.computerRecordOb, this.playRecordOb, this.playerTurnOb])
       .pipe(
-        scan((accumulator, [computerShipNumberOb, playShipNumberOb, turnOb]) => {
+        scan((accumulator, [computerRecordOb, playRecordOb, playerTurnOb]) => {
             return {
-              computerShipNumberOb: accumulator.computerShipNumberOb + computerShipNumberOb,
-              playShipNumberOb: accumulator.playShipNumberOb + playShipNumberOb,
-              turnOb: turnOb
+              computerRecordOb: computerRecordOb,
+              playRecordOb: playRecordOb,
+              playerTurnOb: playerTurnOb
             };
-          }, {computerShipNumberOb: 0, playShipNumberOb: 0, turnOb: false}
+          }, {computerRecordOb: 0, playRecordOb: 0, playerTurnOb: false}
         ))
       .subscribe(value => {
-        alert(value.computerShipNumberOb)
-        alert(value.playShipNumberOb);
-        alert(value.turnOb);
+        if (value.playerTurnOb) {
+          this.playerScores = this.playerScores + value.playRecordOb;
+        } else {
+          this.computerScores = this.computerScores + value.computerRecordOb;
+        }
       });
   }
 
@@ -61,28 +62,26 @@ export class BattleShipGameComponent implements OnInit, AfterViewInit {
 
   }
 
-  private computerPlay():void{
-    const rowData = Math.floor(Math.random() * this.playersComputerBoards.length);
-    const colData = Math.floor(Math.random() * this.playersComputerBoards[0].length);
-    if (this.shipComputerRecords > 0) {
-      this.playersComputerBoards[Number(colData)][Number(rowData)] = this.playersComputerBoards[Number(colData)][Number(rowData)] + 1;
-      this.shipComputerRecords--;
+  private computerPlay(): void {
+    const rowData = Math.floor(Math.random() * this.ComputerBoard.length);
+    const colData = Math.floor(Math.random() * this.ComputerBoard[0].length);
+    if (this.computerPoints > 0) {
+      this.ComputerBoard[Number(colData)][Number(rowData)] = this.ComputerBoard[Number(colData)][Number(rowData)] + 1;
+      this.computerPoints--;
     } else {
-      if(!this.youTurn){
-        const youRecordData = this.playersYouBoards[Number(colData)][Number(rowData)];
-        if (youRecordData != 0 && youRecordData < 90){
-          this.playersYouBoards[Number(colData)][Number(rowData)] = 99;
-          this.computerRecords = this.computerRecords + youRecordData;
-
-          this.turnObservable.next(this.youTurn);
-          this.computerRecordOb.next(this.computerRecords);
+      if (!this.playerTurn) {
+        const youRecordData = this.playerBoard[Number(colData)][Number(rowData)];
+        if (youRecordData != 0 && youRecordData < 90) {
+          this.playerBoard[Number(colData)][Number(rowData)] = 99;
+          this.turnObservable.next(this.playerTurn);
+          this.computerRecordOb.next(youRecordData);
         } else {
-          if(youRecordData < 90){
-            this.playersYouBoards[Number(colData)][Number(rowData)] = 98;
+          if (youRecordData < 90) {
+            this.playerBoard[Number(colData)][Number(rowData)] = 98;
           }
         }
-        this.youTurn = true;
-        this.turnOb.next(this.youTurn);
+        this.playerTurn = true;
+        this.playerTurnOb.next(this.playerTurn);
       }
     }
   }
@@ -93,22 +92,21 @@ export class BattleShipGameComponent implements OnInit, AfterViewInit {
       const colData = clickedDiv.getAttribute('col-data');
       const rowData = clickedDiv.getAttribute('row-data');
       if (colData && rowData) {
-        if (this.shipYouRecords > 0) {
-          this.playersYouBoards[Number(colData)][Number(rowData)] = this.playersYouBoards[Number(colData)][Number(rowData)] + 1;
-          this.shipYouRecords--;
+        if (this.playerPoints > 0) {
+          this.playerBoard[Number(colData)][Number(rowData)] = this.playerBoard[Number(colData)][Number(rowData)] + 1;
+          this.playerPoints--;
         } else {
-          if (this.youTurn) {
-            const computerRecord = this.playersComputerBoards[Number(colData)][Number(rowData)];
+          if (this.playerTurn) {
+            const computerRecord = this.ComputerBoard[Number(colData)][Number(rowData)];
             if (computerRecord != 0 && computerRecord < 90) {
-              this.playersComputerBoards[Number(colData)][Number(rowData)] = 99;
-              this.youRecords = this.youRecords + computerRecord;
-              this.playRecordOb.next(this.youRecords);
+              this.ComputerBoard[Number(colData)][Number(rowData)] = 99;
+              this.playRecordOb.next(computerRecord);
             } else {
-              if(computerRecord < 90){
-                this.playersComputerBoards[Number(colData)][Number(rowData)] = 98;
-                this.youTurn = false;
-                this.turnObservable.next(this.youTurn);
-                this.turnOb.next(this.youTurn);
+              if (computerRecord < 90) {
+                this.ComputerBoard[Number(colData)][Number(rowData)] = 98;
+                this.playerTurn = false;
+                this.turnObservable.next(this.playerTurn);
+                this.playerTurnOb.next(this.playerTurn);
               }
             }
           }
