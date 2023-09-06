957cdfde01ebd568fd71089d66cdf9d3fb4e9abb
tinh.le <tinh.le@formos.com>
2023-09-06 10:36:24 +0700

    fix bug

--- src/app/component/mini-games/battlesship-game/battleship-game.component.ts
+++ src/app/component/mini-games/battlesship-game/battleship-game.component.ts
@@ -1,89 +1,39 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BattleshipGameService} from "@component/mini-games/battlesship-game/battleship-game.service";
-import {BehaviorSubject, combineLatest, debounceTime, scan, Subject} from "rxjs";
+import {debounceTime, Subject} from "rxjs";
+import {GAME_SIZE, NUMBER_OF_SHIP_PARTS} from "@component/mini-games/battlesship-game/battleship-game.constants";
 
 @Component({
   selector: 'app-battleship-game',
   templateUrl: './battleship-game.component.html',
   styleUrls: ['./battleship-game.component.scss']
 })
-export class BattleShipGameComponent implements OnInit, AfterViewInit {
+export class BattleShipGameComponent implements OnInit {
   playerBoard: number[][] = [];
   ComputerBoard: number[][] = [];
-  playerPoints: number = 50;
-  computerPoints: number = 50;
-
+  playerPoints: number = NUMBER_OF_SHIP_PARTS;
+  computerPoints: number = NUMBER_OF_SHIP_PARTS;
   playerScores: number = 0;
   computerScores: number = 0;
   playerTurn: boolean = true;
-
-
   turnObservable: Subject<boolean>;
-  computerRecordOb: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  playRecordOb: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-  playerTurnOb: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private battleshipGameService: BattleshipGameService) {
     this.turnObservable = new Subject<boolean>();
   }
 
   ngOnInit(): void {
-    this.playerBoard = this.battleshipGameService.createTwoDimArray(12, 12);
-    this.ComputerBoard = this.battleshipGameService.createTwoDimArray(12, 12);
+    this.playerBoard = this.battleshipGameService.createTwoDimArray(GAME_SIZE, GAME_SIZE);
+    this.ComputerBoard = this.battleshipGameService.createTwoDimArray(GAME_SIZE, GAME_SIZE);
     this.battleshipGameService.computerSettingShipLocation(this.ComputerBoard, this.computerPoints);
-    this.battleshipGameService.computerSettingShipLocation(this.playerBoard, 49);
+    this.battleshipGameService.computerSettingShipLocation(this.playerBoard, NUMBER_OF_SHIP_PARTS - 1);
     this.computerPoints = 0;
     this.playerPoints = 1;
-
-    this.turnObservable.pipe(debounceTime(1000)).subscribe(value => {
-      this.computerPlay();
-    })
-
-    combineLatest([this.computerRecordOb, this.playRecordOb, this.playerTurnOb])
-      .pipe(
-        scan((accumulator, [computerRecordOb, playRecordOb, playerTurnOb]) => {
-            return {
-              computerRecordOb: computerRecordOb,
-              playRecordOb: playRecordOb,
-              playerTurnOb: playerTurnOb
-            };
-          }, {computerRecordOb: 0, playRecordOb: 0, playerTurnOb: false}
-        ))
+    this.turnObservable
+      .pipe(debounceTime(500))
       .subscribe(value => {
-        if (value.playerTurnOb) {
-          this.playerScores = this.playerScores + value.playRecordOb;
-        } else {
-          this.computerScores = this.computerScores + value.computerRecordOb;
-        }
+        this.computerPlay();
       });
-  }
-
-  ngAfterViewInit(): void {
-
-  }
-
-  private computerPlay(): void {
-    const rowData = Math.floor(Math.random() * this.ComputerBoard.length);
-    const colData = Math.floor(Math.random() * this.ComputerBoard[0].length);
-    if (this.computerPoints > 0) {
-      this.ComputerBoard[Number(colData)][Number(rowData)] = this.ComputerBoard[Number(colData)][Number(rowData)] + 1;
-      this.computerPoints--;
-    } else {
-      if (!this.playerTurn) {
-        const youRecordData = this.playerBoard[Number(colData)][Number(rowData)];
-        if (youRecordData != 0 && youRecordData < 90) {
-          this.playerBoard[Number(colData)][Number(rowData)] = 99;
-          this.turnObservable.next(this.playerTurn);
-          this.computerRecordOb.next(youRecordData);
-        } else {
-          if (youRecordData < 90) {
-            this.playerBoard[Number(colData)][Number(rowData)] = 98;
-          }
-        }
-        this.playerTurn = true;
-        this.playerTurnOb.next(this.playerTurn);
-      }
-    }
   }
 
   playersBoardsClick($event: MouseEvent): void {
@@ -97,21 +47,50 @@ export class BattleShipGameComponent implements OnInit, AfterViewInit {
           this.playerPoints--;
         } else {
           if (this.playerTurn) {
-            const computerRecord = this.ComputerBoard[Number(colData)][Number(rowData)];
-            if (computerRecord != 0 && computerRecord < 90) {
-              this.ComputerBoard[Number(colData)][Number(rowData)] = 99;
-              this.playRecordOb.next(computerRecord);
-            } else {
-              if (computerRecord < 90) {
-                this.ComputerBoard[Number(colData)][Number(rowData)] = 98;
-                this.playerTurn = false;
-                this.turnObservable.next(this.playerTurn);
-                this.playerTurnOb.next(this.playerTurn);
-              }
-            }
+            this.playerPlay(colData, rowData);
           }
         }
       }
     }
   }
+
+  private computerPlay(): void {
+    const rowData = Math.floor(Math.random() * this.ComputerBoard.length);
+    const colData = Math.floor(Math.random() * this.ComputerBoard[0].length);
+    if (this.computerPoints > 0) {
+      this.ComputerBoard[Number(colData)][Number(rowData)] = this.ComputerBoard[Number(colData)][Number(rowData)] + 1;
+      this.computerPoints--;
+    } else {
+      const youRecordData = this.playerBoard[Number(colData)][Number(rowData)];
+      if(youRecordData > 90) {
+        this.computerPlay();
+        return;
+      }
+      if (youRecordData > 0 && youRecordData < 90) {
+        this.playerBoard[Number(colData)][Number(rowData)] = 99;
+        this.computerScores = this.computerScores + youRecordData;
+        this.turnObservable.next(true);
+      } else if (youRecordData == 0){
+        this.playerBoard[Number(colData)][Number(rowData)] = 98;
+        this.playerTurn = true;
+      } else{
+        this.turnObservable.next(true);
+      }
+    }
+  }
+
+  private playerPlay(colData: string, rowData: string): void {
+    const computerRecord = this.ComputerBoard[Number(colData)][Number(rowData)];
+    if (computerRecord != 0 && computerRecord < 90) {
+      this.ComputerBoard[Number(colData)][Number(rowData)] = 99;
+      this.playerScores = this.playerScores + computerRecord;
+    } else {
+      if (computerRecord < 90) {
+        this.ComputerBoard[Number(colData)][Number(rowData)] = 98;
+        this.playerTurn = false;
+        this.turnObservable.next(false);
+      }
+    }
+  }
+
 }
